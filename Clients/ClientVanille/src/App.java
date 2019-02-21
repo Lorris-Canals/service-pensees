e9193afae32d39c9e791310c681e628021ee7cf5
Lorris <lorris.canals@etu.univ-orleans.fr>
2019-02-21 16:14:06 -0500

    Cache SQLite
    La Connexion a la BDD SQLite est fonctionnelle #2

--- Clients/ClientVanille/src/App.java
+++ Clients/ClientVanille/src/App.java
@@ -1,10 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Iterator;
-import java.util.List;
 import accesseur.PenseeDAO;
 import modele.Pensee;
 import outils.Journal;
--- Clients/ClientVanille/src/accesseur/PenseeURL.java
+++ Clients/ClientVanille/src/accesseur/PenseeURL.java
@@ -1,7 +1,7 @@
 package accesseur;
 
 public interface PenseeURL {
-	public static final String DSN = "jdbc::sqlite:C:/sqlite/bases/inspiration.db";
+	public static final String DSN = "jdbc:sqlite:C:/sqlite/bases/cache.inspiration.db";
 	public static final String SQL_ENREGISTRER_PENSEE = "insert into pensee(auteur, message) values(?,?)";
 	public static final String SQL_LISTER_PENSEES = "SELECT * FROM pensee";
 }
